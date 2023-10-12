c85a6d87a737d21a57e491cd7bcd92c2104594fe
Vladislav Simonenko <vlad.simonenko@itp-it.com>
2023-10-12 14:51:05 +0400

    feature(parallax) Coffee. add speed for animations

--- components/organisms/ScrollingEffect/ScrollingEffect.tsx
+++ components/organisms/ScrollingEffect/ScrollingEffect.tsx
@@ -1,55 +1,51 @@
-import { useState, useRef, useLayoutEffect, ReactNode } from "react";
-import {
-  motion,
-  useTransform,
-  useSpring,
-  useReducedMotion,
-  useScroll,
-} from "framer-motion";
+import React, { useEffect, useState } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
-type ParallaxProps = {
-  children: ReactNode;
-  offset?: number;
-};
+export const ScrollingEffect = ({ children }: any): JSX.Element => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+  const [scrollY, setScrollY] = useState(0);
 
-export const ScrollingEffect = ({
-  children,
-  offset = 100,
-}: ParallaxProps): JSX.Element => {
-  const prefersReducedMotion = useReducedMotion();
-  const [elementTop, setElementTop] = useState(0);
-  const [clientHeight, setClientHeight] = useState(0);
-  const ref = useRef(null);
+  useEffect(() => {
+    controls.start({
+      opacity: inView ? 1 : 0, // Прозрачность
+      y: inView ? 0 : 100, // Сдвиг по вертикали
+    });
+  }, [controls, inView]);
 
-  const { scrollY } = useScroll();
+  // Обработчик события скролла
+  const handleScroll = () => {
+    setScrollY(window.scrollY);
+  };
 
-  const initial = elementTop - clientHeight;
-  const final = elementTop + offset;
-
-  const yRange = useTransform(scrollY, [initial, final], [offset, -offset]);
-  const y = useSpring(yRange, { stiffness: 200, damping: 90 });
-
-  useLayoutEffect(() => {
-    const element = ref.current;
-    const onResize = () => {
-      setElementTop(
-        element.getBoundingClientRect().top + window.scrollY || window.scrollY
-      );
-      setClientHeight(window.innerHeight);
-    };
-    onResize();
-    window.addEventListener("resize", onResize);
-    return () => window.removeEventListener("resize", onResize);
-  }, [ref]);
-
-  // Don't parallax if the user has "reduced motion" enabled
-  if (prefersReducedMotion) {
-    return <>{children}</>;
-  }
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <motion.div ref={ref} style={{ y }} className="scrolling-effect">
-      {children}
-    </motion.div>
+    <div>
+      <motion.div
+        ref={ref}
+        initial={{ opacity: 0, y: 100 }}
+        animate={controls}
+        transition={{ duration: 1 }}
+      >
+        {children}
+      </motion.div>
+
+      <div
+        style={{
+          position: "fixed",
+          bottom: "20px",
+          left: "20px",
+          backgroundColor: "white",
+          padding: "10px",
+        }}
+      >
+        ScrollY: {scrollY}
+      </div>
+    </div>
   );
 };
